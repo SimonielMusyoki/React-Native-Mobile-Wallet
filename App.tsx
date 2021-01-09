4c730bb514d9737bddec15e8fd27814b2877328a
SimonielMusyoki <musyoki.mtk3@gmail.com>
2021-01-09 11:47:25 +0300

    Project Setup

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font'
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf')
+  });
+
+  if (!loaded){
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
